662ac9019bfdf047af95d9c451e059ffe1338ca9
chaejungho <chaejungh@gmail.com>
2025-11-11 18:10:45 +0900

    11-11tkk start

---
+++ src/test/java/com/smu/tkk/TkkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smu.tkk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TkkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
