9ec9d5802f82d010ba34632843593e8f735cbce8
KING2 <whoismini@naver.com>
2025-11-13 16:21:19 +0900

    join3

--- src/main/java/com/smu/tkk/UserController.java
+++ src/main/java/com/smu/tkk/UserController.java
@@ -1,14 +1,19 @@
 package com.smu.tkk;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/user")
+@RestController
+@RequiredArgsConstructor
 public class UserController {
     @GetMapping("/join.do")
     public String join(){
         return "user/join";
     }
+
 }
