52731277546ee0e6a5465f526c2cfc9242d19381
dahee97 <yhsmam0817@gmail.com>
2025-11-17 18:11:23 +0900

    서비스 수정

--- src/main/java/com/smu/tkk/service/BoardService.java
+++ src/main/java/com/smu/tkk/service/BoardService.java
@@ -1,39 +1,63 @@
 package com.smu.tkk.service;
 
-import com.smu.tkk.entity.BoardCategory;
 import com.smu.tkk.entity.BoardPost;
+import com.smu.tkk.entity.BoardPostImage;
 import com.smu.tkk.entity.BoardTag;
+import com.smu.tkk.entity.BoardCategory;
 
+import java.sql.SQLException;
 import java.util.List;
 
+/**
+ * 덕질 게시판 서비스
+ *
+ * - 게시글 등록/수정/삭제
+ * - 게시글 상세 / 목록 / 검색
+ * - 이미지 처리
+ * - 태그 / 카테고리 처리
+ */
 public interface BoardService {
 
-    // 1. 게시글 작성
-    BoardPost createPost(BoardPost post,
-                         List<String> tagNames,
-                         List<String> imageUrls);
+    // 1. 게시글 등록
+    boolean register(BoardPost post) throws SQLException, IllegalArgumentException;
 
     // 2. 게시글 수정
-    BoardPost updatePost(Long postId,
-                         BoardPost updateData,
-                         List<String> tagNames,
-                         List<String> imageUrls);
+    boolean modify(BoardPost post) throws SQLException, IllegalArgumentException;
 
-    // 3. 게시글 삭제 (소프트 삭제 가정: deletedYn = 'Y')
-    void deletePost(Long postId, Long memberId);
+    // 3. 게시글 삭제
+    boolean remove(Long postId) throws SQLException;
 
-    // 4. 게시글 상세 조회 (조회수 +1 포함)
-    BoardPost getPostDetail(Long postId);
+    // 4. 게시글 상세 조회
+    BoardPost readOne(Long postId) throws SQLException;
 
-    // 5. 게시글 리스트 조회 (카테고리별)
-    List<BoardPost> getPostListByCategoryCode(String categoryCode);
+    // 5. 게시글 전체 목록 조회
+    List<BoardPost> readAll() throws SQLException;
 
-    // 6. 게시글 검색 (제목 + 내용 + 태그명)
-    List<BoardPost> searchPosts(String keyword);
+    // 6. 게시글 검색 (제목·내용·태그 등은 구현할 때 team rule)
+    List<BoardPost> readByKeyword(String keyword) throws SQLException;
 
-    // 7. 카테고리 전체 조회
-    List<BoardCategory> getAllCategories();
+    // 7. 카테고리별 게시글 조회 (옵션)
+    List<BoardPost> readByCategory(Long categoryId) throws SQLException;
 
-    // 8. 게시글 태그 조회
-    List<BoardTag> getTagsByPost(Long postId);
+    // 8. 게시판 카테고리 전체 조회
+    List<BoardCategory> readAllCategories() throws SQLException;
+
+    // 9. 특정 게시글 이미지 전체 조회
+    List<BoardPostImage> readImages(Long postId) throws SQLException;
+
+    // 10. 게시글에 이미지 추가
+    boolean addImage(Long postId, BoardPostImage image)
+            throws SQLException, IllegalArgumentException;
+
+    // 11. 이미지 삭제
+    boolean removeImage(Long imageId) throws SQLException;
+
+    // 12. 게시글의 태그 목록 조회
+    List<BoardTag> readTagsByPost(Long postId) throws SQLException;
+
+    // 13. 게시글에 태그 추가
+    boolean addTagToPost(Long postId, Long tagId) throws SQLException;
+
+    // 14. 게시글에서 태그 제거
+    boolean removeTagFromPost(Long postId, Long tagId) throws SQLException;
 }
--- src/main/java/com/smu/tkk/service/CommentService.java
+++ src/main/java/com/smu/tkk/service/CommentService.java
@@ -2,14 +2,28 @@ package com.smu.tkk.service;
 
 import com.smu.tkk.entity.BoardComment;
 
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * 댓글 서비스
+ *
+ * - 댓글 등록
+ * - 댓글 삭제
+ * - 댓글 목록 조회
+ * - 대댓글(답글) 등록
+ */
 public interface CommentService {
 
-    // 1. 댓글 작성 (parentCommentId 가 null 이면 일반 댓글, 값 있으면 대댓글)
-    BoardComment addComment(Long postId,
-                            Long memberId,
-                            String content,
-                            Long parentCommentId);
+    // 1. 댓글 등록
+    boolean register(BoardComment comment) throws SQLException, IllegalArgumentException;
 
-    // 2. 댓글 삭제 (작성자만 삭제 가능, soft delete 가정)
-    void deleteComment(Long commentId, Long memberId);
+    // 2. 댓글 삭제
+    boolean remove(Long commentId) throws SQLException;
+
+    // 3. 특정 게시글의 댓글 목록 조회
+    List<BoardComment> readByPost(Long postId) throws SQLException;
+
+    // 4. 대댓글(부모 댓글 기준) 목록 조회 (필요 없다면 구현 안 해도 됨)
+    List<BoardComment> readReplies(Long parentCommentId) throws SQLException;
 }
