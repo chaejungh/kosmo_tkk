1723c6f5f6600bab5209ec06feb824b4641787d1
beomjun <fhqhtvkdnj11@gmail.com>
2025-11-18 09:45:54 +0900

    서비스2

--- src/main/java/com/smu/tkk/service/TradeService.java
+++ src/main/java/com/smu/tkk/service/TradeService.java
@@ -6,11 +6,11 @@ import com.smu.tkk.entity.TradePostImage;
 import java.util.List;
 
 public interface TradeService {
-    TradePost createTradePost(TradePost tradePost); //거래글 작성
+    TradePost registerTradePost(TradePost tradePost); //거래글 작성
     TradePost modifyTradePost(TradePost tradePost); //거래글 수정
     TradePost removeTradePost(TradePost tradePost); //거래글 삭제
     TradePost readOneTradePost(TradePost tradePost); //거래글 상세 조회
     List<TradePost> readlist(TradePost tradePost); //거래글 목록 조회
-    TradePostImage saveTradeimage(TradePost tradePost); //거래글 이미지 저장
-    TradePost changetradepost(TradePost tradePost); //거래 상태 변경
+    TradePostImage saveTradeimage(TradePostImage tradePostImage); //거래글 이미지 저장
+    TradePost modifytradepost(TradePost tradePost); //거래 상태 변경
 }
