f194cc1680856da674baa0dd2a9c02d50d5153c0
chaejungho <chaejungh@gmail.com>
2025-11-20 09:50:09 +0900

    BoardCategory Error solve

--- src/test/java/com/smu/tkk/repository/BoardCategoryRepositoryTest.java
+++ src/test/java/com/smu/tkk/repository/BoardCategoryRepositoryTest.java
@@ -1,6 +1,6 @@
 package com.smu.tkk.repository;
 
-<<<<<<< HEAD
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,29 +14,4 @@ class BoardCategoryRepositoryTest {
     void findById(){
         System.out.println(boardCategoryRepository.findById((long)1));
     }
-
-=======
-import com.smu.tkk.entity.BoardCategory;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Pageable;
-
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-class BoardCategoryRepositoryTest {
-    @Autowired
-    private BoardCategoryRepository boardCategoryRepository;
-    @Test
-    void findById() throws Exception{
-        System.out.println(boardCategoryRepository.findById(1L));
-
-
-
-
-    }
->>>>>>> 3614324cccfe7980251f6bc28fffa7e30fe3ba3d
 }
