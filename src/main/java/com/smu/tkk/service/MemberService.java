58eac4c243dcde4e430128c7b27553c63d878483
chaejungho <chaejungh@gmail.com>
2025-11-24 15:48:45 +0900

    15:48

--- src/main/java/com/smu/tkk/service/MemberService.java
+++ src/main/java/com/smu/tkk/service/MemberService.java
@@ -45,13 +45,13 @@ public interface MemberService {
     // 7. 전체 회원 조회 (관리자/테스트용, 필요 없으면 나중에 삭제)(레파지토리 완료)
     List<Member> readAll(Pageable pageable) throws SQLException;
 
-    // 8. 회원 알림 설정 조회
+    // 8. 회원 알림 설정 조회(레파지토리 완료)
     MemberNotificationSetting readNotificationSetting(Long memberId) throws SQLException;
 
-    // 9. 회원 알림 설정 수정
+    // 9. 회원 알림 설정 수정(레파지토리 완료)
     boolean updateNotificationSetting(MemberNotificationSetting setting) throws SQLException, IllegalArgumentException;
     
-    // 10. Member 이미지 추가
+    // 10. Member 이미지 추가()
     boolean addImage(Long memberId, Member profileImage) throws SQLException, IllegalArgumentException;
     
     // 11. member 이미지 수정
