8197c0bc1fecd6a848552b6ce8aaa02a15f6cde0
Jihyeong <dlwlgud5028@gmail.com>
2025-11-24 16:32:48 +0900

    map search

--- src/main/java/com/smu/tkk/repository/StoreRepository.java
+++ src/main/java/com/smu/tkk/repository/StoreRepository.java
@@ -7,9 +7,7 @@ import java.util.List;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
-    // 이미 있는 메서드들 …
 
-    // 이름 또는 지역명에 키워드가 포함된 매장 20개까지
     List<Store> findTop20ByNameContainingIgnoreCaseOrRegionNameContainingIgnoreCase(
             String nameKeyword,
             String regionKeyword
