43e051e5f859b48f3da4e4d856e616ed56efcc27
chaejungho <chaejungh@gmail.com>
2025-11-24 09:52:19 +0900

    commit

--- src/main/java/com/smu/tkk/repository/BoardPostImageRepository.java
+++ src/main/java/com/smu/tkk/repository/BoardPostImageRepository.java
@@ -3,6 +3,11 @@ package com.smu.tkk.repository;
 import com.smu.tkk.entity.BoardPostImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardPostImageRepository extends JpaRepository<BoardPostImage, Long> {
+import java.sql.SQLException;
+import java.util.List;
 
+public interface BoardPostImageRepository extends JpaRepository<BoardPostImage, Long> {
+    //    // 9. 특정 게시글 이미지 전체 조회
+//    List<BoardPostImage> readImages(Long postId) throws SQLException;
+    List<BoardPostImage> findAllByPostId(Long postId) throws SQLException;
 }
---
+++ src/test/java/com/smu/tkk/repository/BoardPostImageRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.smu.tkk.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardPostImageRepositoryTest {
+
+    @Test
+    void findAllByPostId() {
+    }
+}
