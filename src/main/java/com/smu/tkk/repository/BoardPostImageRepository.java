33ef02d98d15b1dd20dee07f46b812b68333bb1b
chaejungho <chaejungh@gmail.com>
2025-11-24 10:06:40 +0900

    테스트

--- src/main/java/com/smu/tkk/repository/BoardPostImageRepository.java
+++ src/main/java/com/smu/tkk/repository/BoardPostImageRepository.java
@@ -1,13 +1,15 @@
 package com.smu.tkk.repository;
 
 import com.smu.tkk.entity.BoardPostImage;
+import org.hibernate.query.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
 import java.sql.SQLException;
 import java.util.List;
 
 public interface BoardPostImageRepository extends JpaRepository<BoardPostImage, Long> {
     //    // 9. 특정 게시글 이미지 전체 조회
 //    List<BoardPostImage> readImages(Long postId) throws SQLException;
-    List<BoardPostImage> findAllByPostId(Long postId) throws SQLException;
+    List<BoardPostImage> findAllByPostId(Long postId ) throws SQLException;
 }
--- src/test/java/com/smu/tkk/repository/BoardPostImageRepositoryTest.java
+++ src/test/java/com/smu/tkk/repository/BoardPostImageRepositoryTest.java
@@ -1,12 +1,18 @@
 package com.smu.tkk.repository;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class BoardPostImageRepositoryTest {
-
+    @Autowired
+    BoardPostImageRepository boardPostImageRepository;
     @Test
-    void findAllByPostId() {
+    void findAllByPostId() throws SQLException {
+        System.out.println(boardPostImageRepository.findAllByPostId(1L));
     }
 }
