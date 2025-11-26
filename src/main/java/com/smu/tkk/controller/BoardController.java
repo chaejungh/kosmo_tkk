60f09efa54cfaef7f550d737a54c43719efd1edc
dahee97 <yhsmam0817@gmail.com>
2025-11-26 11:46:40 +0900

    게시판 컨트롤러 수정

--- src/main/java/com/smu/tkk/controller/BoardController.java
+++ src/main/java/com/smu/tkk/controller/BoardController.java
@@ -11,11 +11,17 @@ public class BoardController {
      * 자유게시판 리스트
      * 예) /free/1/list.do
      */
-    @GetMapping("/free/{memberId}/list.do")
+    @GetMapping("/free/list.do")
     public String freeBoardList(@PathVariable("memberId") Long memberId) {
         // TODO: memberId 사용해서 내가 쓴 글, 권한 등 나중에 서비스 붙이면 됨
         // 일단은 게시판 리스트 화면만 보여주자.
         return "board/board_list";   // 이미 사용하던 템플릿 이름 기준
+    }
+    @GetMapping("/mcboard/list.do")
+    public String mcBoardList(@PathVariable("memberId") Long memberId) {
+        // TODO: memberId 사용해서 내가 쓴 글, 권한 등 나중에 서비스 붙이면 됨
+        // 일단은 게시판 리스트 화면만 보여주자.
+        return "board/mcboard_list";   // 이미 사용하던 템플릿 이름 기준
     }
 
     /**
@@ -24,6 +30,6 @@ public class BoardController {
      */
     @GetMapping("/board")
     public String legacyBoardRoot() {
-        return "redirect:/free/1/list.do";
+        return "redirect:/mcboard/list.do";
     }
 }
