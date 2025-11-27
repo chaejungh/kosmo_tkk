63bbe2a6c09286c15e93f816af6a1cda99e3f125
chaejungho <chaejungh@gmail.com>
2025-11-27 16:41:49 +0900

    To all board hotboard created

--- src/main/java/com/smu/tkk/controller/BoardController.java
+++ src/main/java/com/smu/tkk/controller/BoardController.java
@@ -23,12 +23,6 @@ public class BoardController {
      * 자유게시판 리스트
      * 예) /free/1/list.do
      */
-    @GetMapping("/free/{memberId}/list.do")
-    public String freeBoardList(@PathVariable("memberId") Long memberId) {
-        // TODO: memberId 사용해서 내가 쓴 글, 권한 등 나중에 서비스 붙이면 됨
-        // 일단은 게시판 리스트 화면만 보여주자.
-        return "mcboard_list";   // 이미 사용하던 템플릿 이름 기준
-    }
     @GetMapping("/mcboard/{memberId}/list.do")
     public String mcBoardList(@PathVariable("memberId") Long memberId, Model model, Pageable pageable) throws SQLException {
         // TODO: memberId 사용해서 내가 쓴 글, 권한 등 나중에 서비스 붙이면 됨
@@ -57,9 +51,16 @@ public class BoardController {
         // 일단은 게시판 리스트 화면만 보여주자.
         Long categoryId=2L;//카테고리아이디 2== cosplayboard
         Page<BoardPost> posts = boardService.readByCategory(categoryId,pageable);
+        // 2) 현재 게시판 인기글 TOP5  (왼쪽)
+        List<BoardPost> hotCurrentBoard = boardService.getHotPostsInCategory(categoryId);
+
+        // 3) 전체 게시판 인기글 TOP5   (오른쪽)
+        List<BoardPost> hotAllBoard = boardService.getHotPostsAll();
 
         model.addAttribute("memberId", memberId);
         model.addAttribute("posts", posts); // ★ 타임리프에서 ${posts}로 사용
+        model.addAttribute("hotCurrentBoard", hotCurrentBoard);//현재게시판기준 핫글
+        model.addAttribute("hotAllBoard", hotAllBoard);//전체게시판기준 핫글
         return "board/cosplayboard_list";   // 이미 사용하던 템플릿 이름 기준
     }
     @GetMapping("/freeboard/{memberId}/list.do")
@@ -68,9 +69,15 @@ public class BoardController {
         // 일단은 게시판 리스트 화면만 보여주자.
         Long categoryId=3L;//카테고리아이디 3== freeboard
         Page<BoardPost> posts = boardService.readByCategory(categoryId,pageable);
+        // 2) 현재 게시판 인기글 TOP5  (왼쪽)
+        List<BoardPost> hotCurrentBoard = boardService.getHotPostsInCategory(categoryId);
 
+        // 3) 전체 게시판 인기글 TOP5   (오른쪽)
+        List<BoardPost> hotAllBoard = boardService.getHotPostsAll();
         model.addAttribute("memberId", memberId);
         model.addAttribute("posts", posts); // ★ 타임리프에서 ${posts}로 사용
+        model.addAttribute("hotCurrentBoard", hotCurrentBoard);//현재게시판기준 핫글
+        model.addAttribute("hotAllBoard", hotAllBoard);//전체게시판기준 핫글
         return "board/freeboard_list";   // 이미 사용하던 템플릿 이름 기준
     }
 
