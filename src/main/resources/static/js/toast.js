7f3be4a7a918be7f4a0947e12848182c51f86bc0
KING2 <whoismini@naver.com>
2025-12-03 22:23:55 +0900

    1203 alarm fianl modify

--- src/main/resources/static/js/toast.js
+++ src/main/resources/static/js/toast.js
@@ -1,19 +1,8 @@
-
-
-///session 멤버아이디로 변경 ///
-    const memberId=1;
+if (memberId) {
     const es = new EventSource(`/alarm/stream/${memberId}`);
-    console.log(es);
-    es.onmessage = (event) => {
-    // showToast(event.data); // 서버에서 보낸 메시지를 토스트로 띄움
-    console.log(event.data)
-
-};
 
     es.addEventListener("notification", (event) => {
-    // showToast(event.data);
-    console.log(event.data)
-
-});
-
-
+        showToast(event.data);   // 드디어 토스트 뜸!
+        console.log(event.data);
+    });
+}
